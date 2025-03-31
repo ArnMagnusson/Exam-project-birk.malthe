36ae8051ac4befac5aafcc5b5628b060aab1e7fe
Arn <Birksanddahlmortensen@gmail.com>
2025-03-31 10:26:00 +0200

    Created damage handler for enemies.

--- scripts/playerscripts/movement_controller.gd
+++ scripts/playerscripts/movement_controller.gd
@@ -76,10 +76,13 @@ func _on_enemyhit_area_entered(area): #Enemy hit player
 			var damage= area.get_damage()
 			#var damage_type = area.get_damage_type() If we need it further in.
 			health -= damage
-		
-	if health <= 0:
-		death() #calls death function
-		
+			Take_damage()
+			
+func Take_damage():
+	health <= 0
+	print("Damage taken")
+	death()
+
 func _on_lungedetection_area_entered(area): #lunge
 	pass # Replace with function body.
 
