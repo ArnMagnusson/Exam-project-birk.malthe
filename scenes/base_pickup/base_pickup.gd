375d5a3b89ca8c0ad464ff896c18fdc9bd678655
Arn <Birksanddahlmortensen@gmail.com>
2025-04-01 12:35:22 +0200

    power up stuff

--- scenes/base_pickup/base_pickup.gd
+++ scenes/base_pickup/base_pickup.gd
@@ -1,7 +1,9 @@
 extends Node2D
 signal Pick_upped
 
-func _on_pickup_area_area_entered(area):
+
+func _on_pickup_area_body_entered(body):
+	if body.is_in_group("Player"):
 		print("BOOST")
 		emit_signal("Pick_upped")
 		queue_free()
