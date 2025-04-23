da51d832b9eb1d5bee8065af586c9ee44a491911
MaltheAA <malthealexander@gmail.com>
2025-04-23 19:19:07 +0200

    treasure fix?

--- chest.gd
+++ chest.gd
@@ -2,4 +2,8 @@ extends Node2D
 
 func _on_chest_area_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
+		var treasurescene = preload("res://treasure.tscn")
+		var instance = treasurescene.instantiate()
+		instance.global_position = global_position
+		get_parent().add_child(instance)
 		queue_free()
